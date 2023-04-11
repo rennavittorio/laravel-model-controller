6e547adc02f5a953310e8a58418729e30033d5eb
rennavittorio <renna.vittorio@gmail.com>
2023-04-11 16:04:07 +0200

    add: add cards to show movie infos style: style cards and body (basic colors)

--- resources/views/Guest/homepage.blade.php
+++ resources/views/Guest/homepage.blade.php
@@ -5,20 +5,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
 </head>
 <body>
-    
-    <ul>
 
-        @foreach ($movies as $movie)
-    
-        <li>
-            {{ $movie->title }} -- {{ $movie->id }}
-        </li>
-            
-        @endforeach
+    <div class="container">
 
-    </ul>
+        <h1 class="text-center text-white fs-1">
+            Boolflix && Chill
+        </h1>
+
+        <ul class="grid-dis mt-3">
+    
+            @foreach ($movies as $movie)
+        
+                <div class="card">
+                    <div class="card-body d-flex flex-column">
+                        <h5 class="card-title"> {{ $movie->title }} </h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary"> {{ $movie->original_title }} </h6>
+                        <p class="card-text mt-auto"> {{ $movie->nationality }} </p>
+                        <div class="info-wrapper">
+                            <span class="card-info"> {{ $movie->date }} </span> //
+                            <span class="card-info vote"> {{ $movie->vote }} </span>
+                        </div>
+                    </div>
+                </div>
+                
+            @endforeach
+    
+        </ul>
+
+    </div>
+    
 
 
 </body>
