dc8135b3dba9e22c640ade90ed684de7f2ad21ed
rennavittorio <renna.vittorio@gmail.com>
2023-04-11 15:40:36 +0200

    start: start new project and complete file setup add: add db connection, controller and movie-model

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function homepage()
+    {
+
+        $movies = Movie::all();
+        // dd($movies);
+
+        $data = [
+            'movies' => $movies,
+        ];
+
+
+        return view('guest.homepage', $data);
+    }
+}
